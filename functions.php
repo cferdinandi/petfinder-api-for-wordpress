516764cb7c5c200f2f4f075fa055081bdc3d0080
Chris Ferdinandi <hello@gomakethings.com>
2013-03-11 07:27:15 -0400

    Added wp_remote_get function for better cross-server compatibility

--- functions.php
+++ functions.php
@@ -22,11 +22,16 @@ function petf_shelter_list() {
     $shelter_id = 'xxxx'; // Change to your shelter ID
     $url = "http://api.petfinder.com/shelter.getPets?key=" . $api_key . "&count=" . $count . "&id=" . $shelter_id . "&status=A&output=full"; // API call
 
-    // If URL exists
+    // Get URL and XML body
+    // (WP function that uses whatever method your server supports)
+    $xml_response = wp_remote_get($url);
+    $xml_body = wp_remote_retrieve_body($xml_response);
+
+    // If the URL exists
     // (Prevents WordPress from displaying private API info publicly if link is broken)
-    if ($url) {
-        // Request shelter data
-        $xml = simplexml_load_file( $url );
+    if ($xml_body) {
+        // Convert to string
+        $xml = simplexml_load_string($xml_body);
     }
 
 
